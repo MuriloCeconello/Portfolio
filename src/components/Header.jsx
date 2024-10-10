12300a1275b122ee2408d97024ca65032717ae48
MuriloCeconello <ceconellomurilo@gmail.com>
2024-10-10 09:11:10 -0300

    update portfolio file

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <header className="bg-sky-800 flex justify-between conta">
+            <h1 className=""><a href="#">MURILOCECONELLO</a></h1>
+            <nav className="">
+                <ul className="fixed flex">
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Projetos</a></li>
+                    <li><a href="#">Certificados</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header;
