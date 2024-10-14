35c7f156518af68659047c567be0f0db08282a30
MuriloCeconello <ceconellomurilo@gmail.com>
2024-10-14 13:33:09 -0300

    create responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 function Header() {
     return (
-        <header className="bg-lightmode-background-default dark:bg-darkmode-background-default flex justify-between items-center p-10 fixed w-screen">
-            <div className="flex font-Salsa items-center fixed">
+        <header className="bg-lightmode-background-default dark:bg-darkmode-background-default flex justify-between items-center p-10 fixed w-screen max-md:justify-center">
+            <div className="flex font-Salsa items-center fixed text-center">
                 <h1 className="text-lightmode-font-color dark:text-default-black font-bold text-3xl"><a href="#">MURILO</a></h1>
                 <h1 className="text-default-white font-bold text-3xl dark:text-darkmode-font-color"><a href="#">CECONELLO</a></h1>
             </div>
-            <nav className="font-Poppins flex fixed right-20 items-center">
+            <nav className="font-Poppins fixed right-20 items-center max-md:hidden">
                 <ul className="flex text-lightmode-font-color dark:text-darkmode-font-color font-normal">
-                    <li><a href="#sobre" className="ml-16 text-lg transition duration-500">Sobre</a></li>
-                    <li><a href="#projetos" className="ml-16 text-lg transition duration-500">Projetos</a></li>
-                    <li><a href="#certificados" className="ml-16 text-lg transition duration-500">Certificados</a></li>
+                    <li><a href="#sobre" className="ml-10 text-lg transition duration-500">Sobre</a></li>
+                    <li><a href="#projetos" className="ml-10 text-lg transition duration-500">Projetos</a></li>
+                    <li><a href="#certificados" className="ml-10 text-lg transition duration-500">Certificados</a></li>
                 </ul>
             </nav>
         </header>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,24 +2,25 @@ function Main() {
     return (
         <main>
             <section className="flex justify-around items-center bg-lightmode-background-color-2 dark:bg-darkmode-background-color-2 w-full h-screen p-10">
-                <div className="text-justify w-4/12">
-                    <h1 className="text-center font-Chakra font-bold text-5xl text-lightmode-font-color dark:text-darkmode-font-color">Olá! Me chamo Murilo </h1>
-                    <h2 className="font-Poppins text-lightmode-font-color dark:text-darkmode-font-color text-xl">Tenho 18 anos e iniciei minha trajetória no desenvolvimento aos 16. Atualmente, atuo como desenvolvedor front-end, mas pretendo me tornar um desenvolvedor full-stack. Meus hobbies incluem programar, ouvir música, jogar videogame e, ocasionalmente, cozinhar. Além disso, tenho o desejo de conhecer diversos países e aprender vários idiomas.</h2>
+                <div className="text-justify w-4/12 max-lg:w-screen max-xl:w-7/12">
+                    <h1 className="text-center font-Chakra font-bold text-5xl text-lightmode-font-color dark:text-darkmode-font-color max-sm:text-4xl">Olá! Me chamo Murilo </h1>
+                    <h2 className="font-Poppins text-lightmode-font-color dark:text-darkmode-font-color text-xl max-xl:text-lg">Tenho 18 anos e iniciei minha trajetória no desenvolvimento aos 16. Atualmente, atuo como desenvolvedor front-end, mas pretendo me tornar um desenvolvedor full-stack. Meus hobbies incluem programar, ouvir música, jogar videogame e, ocasionalmente, cozinhar. Além disso, tenho o desejo de conhecer diversos países e aprender vários idiomas.</h2>
                 </div>
-                <img className="w-4/12" src="images/dev.png" alt="Developer" />
+                <img className="w-4/12 max-xl:hidden" src="images/dev.png" alt="Developer"/>
             </section>
             <section id="sobre" className="bg-lightmode-background-default dark:bg-darkmode-background-default w-full p-10">
-                <h1 className="text-center text-4xl font-Chakra font-bold text-lightmode-font-color dark:text-darkmode-font-color pt-10">Sobre</h1>
-                <div className="flex justify-around my-14">
-                    <div className="text-justify w-3/12">
+                <h1 className="text-center text-4xl font-Chakra font-bold text-lightmode-font-color dark:text-darkmode-font-color pt-10 max-lg:pt-1">Sobre</h1>
+                <div className="flex justify-around mt-14 max-lg:flex-col gap-16">
+                    <div className="text-justify w-3/12 max-lg:w-full lg:w-6/12">
                         <h1 className="text-center text-2xl font-Chakra font-semibold text-lightmode-font-color dark:text-darkmode-font-color">Sobre:</h1>
-                        <h2 className="text-base font-Poppins text-lightmode-font-color dark:text-darkmode-font-color text-justify"><p className="indent-5">Meu primeiro contato com o desenvolvimento web ocorreu em um curso de Análise e Desenvolvimento de Sistemas, realizado na escola SENAI “Álvares Romi”, no ano de 2022, com término em julho de 2023. Inicialmente, não sabia se continuaria na área de tecnologia e, por isso, passei um ano sem ter contato com códigos ou programação. Ao final desse período, decidi seguir essa profissão.</p>
+                        <h2 className="text-base font-Poppins text-lightmode-font-color dark:text-darkmode-font-color text-justify">
+                            <p className="indent-5">Meu primeiro contato com o desenvolvimento web ocorreu em um curso de Análise e Desenvolvimento de Sistemas, realizado na escola SENAI “Álvares Romi”, no ano de 2022, com término em julho de 2023. Inicialmente, não sabia se continuaria na área de tecnologia e, por isso, passei um ano sem ter contato com códigos ou programação. Ao final desse período, decidi seguir essa profissão.</p>
                             <p className="indent-5">Realizei o curso de HTML5 e CSS3 do <a href="https://www.cursoemvideo.com/" className="text-lightmode-font-color dark:text-darkmode-background-color-2 font-extrabold hover:underline" target="_blank">CursoemVideo</a>, seguido pelo curso de JavaScript. Devido à falta de conhecimento neste último, optei por realizar um curso na escola SENAI, onde estou atualmente matriculado.</p>
                             <p className="indent-5">Para o futuro, pretendo me tornar desenvolvedor full-stack com foco em front-end. Tenho a intenção de aprimorar meus conhecimentos em React.js e Tailwind CSS, além de aprender e me especializar em Angular, Vue, Python, PHP, entre outras tecnologias. Acredito que conhecimento nunca é demais.</p></h2>
                     </div>
                     <div>
                         <h1 className="text-2xl text-center font-Chakra font-semibold text-lightmode-font-color dark:text-darkmode-font-color mb-5">Hard Skills:</h1>
-                        <div className="flex justify-around gap-10">
+                        <div className="flex justify-around gap-10 max-lg:gap-0">
                             <div className="bg-lightmode-card-color dark:bg-darkmode-card-color hover:bg-default-white dark:hover:bg-default-grey hover:scale-105 hover:shadow-xl p-2 rounded-md transition duration-300">
                                 <img className="w-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" alt="HTML5 logo" />
                             </div>
@@ -31,14 +32,13 @@ function Main() {
                 </div>
             </section>
             <section id="projetos" className="w-full p-10 bg-default-white">
-                <h1 className="font-Chakra text-center font-bold text-4xl text-lightmode-font-color dark:text-default-black pt-10">Projetos</h1>
-                <div>
-                    <div className="flex flex-wrap justify-around mt-10">
+                <h1 className="font-Chakra text-center font-bold text-4xl text-lightmode-font-color dark:text-default-black pt-10
+                mb-10">Projetos</h1>
+                <div className="flex justify-center">
+                    <div className="flex flex-wrap justify-around gap-y-10 xl:w-10/12 2xl:w-8/12 min-[1230px]:w-10/12 max-[1400px]:w-10/12 min-[1463px]:w-8/12 min-[1809px]:w-7/12">
                         <div className="bg-lightmode-card-color  dark:bg-darkmode-card-color w-72 h-56 rounded-xl"></div>
                         <div className="bg-lightmode-card-color  dark:bg-darkmode-card-color w-72 h-56 rounded-xl"></div>
                         <div className="bg-lightmode-card-color  dark:bg-darkmode-card-color w-72 h-56 rounded-xl"></div>
-                    </div>
-                    <div className="flex flex-wrap justify-around mt-10">
                         <div className="bg-lightmode-card-color  dark:bg-darkmode-card-color w-72 h-56 rounded-xl"></div>
                         <div className="bg-lightmode-card-color  dark:bg-darkmode-card-color w-72 h-56 rounded-xl"></div>
                         <div className="bg-lightmode-card-color  dark:bg-darkmode-card-color w-72 h-56 rounded-xl"></div>
@@ -47,7 +47,7 @@ function Main() {
             </section>
             <section id="certificados" className="bg-lightmode-background-default dark:bg-darkmode-background-default w-full p-20">
                 <h1 className="text-center text-lightmode-font-color dark:text-darkmode-font-color font-Chakra text-4xl font-bold">Certificados</h1>
-                <div className="flex flex-warp justify-around mt-10">
+                <div className="flex justify-around mt-10 max-lg:flex-col max-lg:gap-y-10 max-lg:items-center lg:gap-x-5">
                     <div className="bg-lightmode-card-color  dark:bg-darkmode-card-color w-72 h-48 rounded-xl"></div>
                     <div className="bg-lightmode-card-color  dark:bg-darkmode-card-color w-72 h-48 rounded-xl"></div>
                     <div className="bg-lightmode-card-color  dark:bg-darkmode-card-color w-72 h-48 rounded-xl"></div>
