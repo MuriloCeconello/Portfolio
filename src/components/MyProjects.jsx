be4f66195e778807560d68b1676407478469fe11
MuriloCeconello <ceconellomurilo@gmail.com>
2024-10-25 17:17:31 -0300

    updating MyProjects.jsx

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,6 +1,6 @@
 function MyProjects() {
     return (
-        <div className="bg-lightmode-card-color  dark:bg-darkmode-card-color w-72 h-56 rounded-xl hover:bg-opacity-70 transition ease duration-500">
+        <div className="bg-lightmode-card-color  dark:bg-darkmode-card-color w-72 h-56 rounded-xl hover:bg-opacity-70 transition ease duration-500 max-lg:hidden">
             <div className="flex flex-col items-center justify-center group/card visible group-hover/card:visible h-full font-Poppins gap-y-7">
                 <div className="flex flex-col items-center text-lightmode-font-color dark:text-darkmode-background-color-2">
                     <h3 className="group/card invisible group-hover/card:visible text-lg">Nome do Projeto</h3>
