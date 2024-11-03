43f79b8ea9356556cfd2cad38e6feb659a8c95f7
MuriloCeconello <ceconellomurilo@gmail.com>
2024-11-03 01:06:33 -0300

    updating components folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Header from './components/Header'
+import Header from '../src/components/Header/Header'
 import Main from '../src/components/Main/Main'
-import Footer from './components/Footer'
+import Footer from '../src/components/Footer/Footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
