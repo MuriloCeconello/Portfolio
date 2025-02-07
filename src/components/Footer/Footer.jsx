6122ca134bc24c792c9529a8dbbde8492e18c8ed
MuriloCeconello <ceconellomurilo@gmail.com>
2025-02-07 12:58:18 -0300

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,10 @@
 function Footer() {
     return (
         <footer className="bg-lightmode-background-color-2 dark:bg-darkmode-background-color-2 p-6 max-sm:flex-col max-sm:gap-y-3">
-            <div className="flex justify-between">
+            <div className="flex justify-between max-md:flex-col max-md:items-center max-md:gap-y-5">
                 <div className="text-lg font-Chakra font-bold text-lightmode-font-color dark:text-darkmode-font-color">&copy;Desenvolvido por Murilo Ceconello
                 </div>
-                <div className="flex gap-5">
+                <div className="flex gap-5 max-md:flex-wrap">
                     <a href="https://github.com/MuriloCeconello" target="_blank"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="GitHub link" /></a>
                     <a href="https://www.instagram.com/ceconellomurilo/" target="_blank"><img src="https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white" alt="Instagram link" /></a>
                     <a href="https://www.linkedin.com/in/muriloceconello/" target="_blank"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="Linkedin link" /></a>
