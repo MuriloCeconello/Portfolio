e8ea15194edcd63504ed2c8a167e5de88022cfc6
MuriloCeconello <ceconellomurilo@gmail.com>
2025-06-24 13:23:21 -0300

    alteração de idade na introdução

--- src/components/Main/Introduction.jsx
+++ src/components/Main/Introduction.jsx
@@ -4,7 +4,7 @@ function Introduction() {
             <div className="text-justify w-4/12 max-lg:w-screen max-xl:w-7/12">
                 <div className="flex flex-col items-center xl:w-full">
                     <h1 className="text-center font-Chakra font-bold text-5xl text-lightmode-font-color dark:text-darkmode-font-color max-sm:text-4xl">Olá! Me chamo Murilo </h1>
-                    <h2 className="font-Poppins text-lightmode-font-color dark:text-darkmode-font-color text-xl max-md:w-full max-lg:w-9/12 xl:w-11/12">Tenho 18 anos e iniciei minha trajetória com desenvolvimento web aos 16. Atualmente, atuo como desenvolvedor front-end, mas futuramente pretendo me tornar desenvolvedor full-stack.</h2>
+                    <h2 className="font-Poppins text-lightmode-font-color dark:text-darkmode-font-color text-xl max-md:w-full max-lg:w-9/12 xl:w-11/12">Tenho 19 anos e iniciei minha trajetória no desenvolvimento web aos 16. Atualmente, estou em busca de uma oportunidade na área de front-end.</h2>
                 </div>
             </div>
         </>
