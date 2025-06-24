90d1777df6148a33407e5cb09a76d235dde800f0
MuriloCeconello <ceconellomurilo@gmail.com>
2025-06-24 12:29:37 -0300

    update hardskills.jsx

--- src/components/Main/HardSkills.jsx
+++ src/components/Main/HardSkills.jsx
@@ -1,8 +1,7 @@
 function HardSkills({ img, name }) {
     return (
-        <div className="bg-lightmode-card-color dark:bg-darkmode-card-color hover:bg-white dark:hover:bg-gray-500 hover:scale-105 hover:shadow-xl 
-        p-2 rounded-md transition duration-300">
-            <img className="w-20" src={img} alt={name} />
+        <div className="bg-lightmode-card-color dark:bg-darkmode-card-color hover:bg-white dark:hover:bg-gray-500 hover:scale-105 hover:shadow-xl p-2 rounded-md transition duration-300">
+            <img className="w-20 " src={img} alt={name} />
         </div>
     )
 }
